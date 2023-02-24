cb549af4e4fec65a6866b7d45dc90879c121080c
Rocco Nicholls <rocco11nicholls@gmail.com>
2023-02-23 22:55:31 -0700

    Dirt generation

--- game/board.rb
+++ game/board.rb
@@ -5,7 +5,15 @@ class Board < InfiniBoard
   def self.create
     new(->(x, y) {
       return Block.base_from_type(:air) if y < GROUND_LEVEL # negative is up, positive is down
-      Calc.rand_ratio(0.15) ? Block.base_from_type(:ore_invis) : Block.base_from_type(:stone_invis)
+
+      dirt = (95 - ((GROUND_LEVEL + y) * 15)).clamp(5, 95) # 95% at surface, 15% less each layer down - minimum of 5%
+      if Calc.rand_percent(dirt)
+        Block.base_from_type(:dirt_invis)
+      elsif Calc.rand_percent(15)
+        Block.base_from_type(:ore_invis)
+      else
+        Block.base_from_type(:stone_invis)
+      end
     })
   end
 
